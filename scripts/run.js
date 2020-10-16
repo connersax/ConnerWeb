904e3bf933b9ad1824a1bdbd6b8206ab33ebcaaa
Conner Sax <conairisawesome@gmail.com>
2020-10-15 22:10:20 -0400

    the start of something new

---
+++ scripts/run.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+  el: "#vanta-fog",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0x77ff,
+  midtoneColor: 0x800191,
+  lowlightColor: 0xff00,
+  baseColor: 0x0,
+  blurFactor: 0.90,
+  speed: 0.40
+})
